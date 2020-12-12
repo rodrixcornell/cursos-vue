50e01947e44a2339459ce3e1476531bd59ad1670
Rodrigo Cabral <rodrixcornell@gmail.com>
2020-12-12 10:27:14 -0400

    No ramo master
    - Componente Navbar;
    
    - Mudanças a serem submetidas:
    	new file:   .editorconfig
    	modified:   src/App.vue
    	new file:   src/components/Navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
   <div>
-    <form @submit.prevent="salvarForm" method="post">
-      <input type="text" v-model="nome" autocomplete="off">
-      <b-button type="submit" variant="success">Salvar</b-button>
-      <b-button variant="danger" @click="mostrarNome">Mostrar Nome</b-button>
-    </form>
-
-    <h1>Nome: {{ nome }}</h1>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
+
 export default {
   name: 'App',
+  components: {
+    Navbar
+  },
   data () {
     return {
-      nome: ''
-    }
-  },
-  methods: {
-    salvarForm () {
-      alert(this.nome)
-    },
-    mostrarNome () {
-      alert(`o nome digitado é: ${this.nome}`)
     }
   }
 }
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Cursos Legais</b-navbar-brand>
+  
+  <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+      <b-dropdown-item href="#">Profile</b-dropdown-item>
+    </b-navbar-nav>
+
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
